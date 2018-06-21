7edfd916854a1f8b9bf9cb69bd565f65c49a0286
Ashanti Williams <ashanti.williams.16@tfacorps.org>
2018-06-21 18:49:54 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,7 @@
 #write out your code here
 coins = [":quarters",":dimes",":nickels", ":pennies"]
 denominations = [25, 10, 5, 1]
-def least_coins(cents, coins, coins_result = {})
+def least_coins(cents)
   coin = coins.shift
   coins_result[denomination] = amount/denominations
 
