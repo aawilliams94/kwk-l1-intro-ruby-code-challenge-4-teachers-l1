cb17e6eb859687a315bae5ff8b92a718000487f7
Ashanti Williams <ashanti.williams.16@tfacorps.org>
2018-06-21 18:51:24 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -2,8 +2,8 @@
 coins = [":quarters",":dimes",":nickels", ":pennies"]
 denominations = [25, 10, 5, 1]
 def least_coins(cents)
-  coins = coins.shift
-  coins_result[denomination] = amount/denominations
+  coin = coins.shift
+  coins_result[denomination] = amount/coin
 
   if coins.empty?
     denominations_result
